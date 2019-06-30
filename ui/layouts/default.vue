0dffb24b0431bc5bd6949545d8af52c9fab80415
Ivan Bagrov <panthevm@Panthevm.Dlink>
2019-06-30 21:05:22 +0300

    [view] layouts setting

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
+  <v-app white>
+    <v-navigation-drawer v-model="drawer" fixed temporary app>
       <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
+        <v-list-tile router :to="item.to" :key="i" v-for="(item, i) in items" exact>
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
@@ -22,79 +10,66 @@
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-group no-action sub-group value="true">
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-title>Товары</v-list-tile-title>
+            </v-list-tile>
+          </template>
+          <v-list-tile router :to="categ.to" v-for="(categ, i) in category" :key="i">
+            <v-list-tile-title v-text="categ.title"></v-list-tile-title>
+            <v-list-tile-action>
+              <v-icon v-html="categ.icon"></v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+
+    <v-toolbar fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
+
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+
+    <v-footer height="auto">
+      <v-layout justify-center row wrap>
+        <v-btn v-bind:href="item.href" v-for="(item, i) in social" :key="i" flat round>
+          <v-title v-text="item.title"></v-title>
+        </v-btn>
+        <v-flex py-3 text-xs-center xs12>
+          &copy;2019 —
+          <strong>Marsell</strong>
+        </v-flex>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  data () {
+    return {
+      drawer: false,
+      category: [
+        { icon: 'apps', title: 'Кровати', to: '/' },
+        { icon: 'apps', title: 'Комоды', to: '/inspire' },
+        { icon: 'apps', title: 'Шкафы', to: '/@' }
+      ],
+      items: [
+        { icon: 'apps', title: 'Главаня страница', to: '/' }
+      ],
+      social: [
+        { title: 'Инстраграм', href: 'https://vk.com/id391422246' },
+        { title: 'Вконтакте', href: 'https://vk.com/id391422246' }
+      ],
+      title: 'MARSELL'
     }
   }
+}
 </script>
